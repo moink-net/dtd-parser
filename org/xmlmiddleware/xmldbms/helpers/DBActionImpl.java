e36e68b9e85ac499a4f7bf7d31d766b6017fafc9
sean0x77 <>
2001-12-13 00:45:21 +0000

    *** empty log message ***

--- org/xmlmiddleware/xmldbms/helpers/DBActionImpl.java
+++ org/xmlmiddleware/xmldbms/helpers/DBActionImpl.java
@@ -12,63 +12,97 @@ import org.xmlmiddleware.xmldbms.maps.utils.*;
 // updateColumns does not include any primary key or unique key columns.
 // (State this in the comments.)
 
-abstract class DBActionImpl
+abstract class DBActionBase
     implements DBAction
 {
-    public static final int COMMIT_AFTERINSERT = 1;
-    public static final int COMMIT_AFTERDOCUMENT = 2;
-    public static final int COMMIT_NONE = 3;
-    public static final int COMMIT_NOTRANSACTIONS = 4;
+    protected Connection m_connection;
+    protected DMLGenerator m_dml;
+    protected int m_commitMode;
 
     DBActionImpl(DataSource dataSource, String user, String password)
         throws SQLException
     {
         m_connection = dataSource.getConnection(user, password);
         m_dml = new DMLGenerator(m_connection.getMetaData());
+
+        // TODO: Shouldn't the commit modes be moved to another class?
+        m_commitMode = DOMToDBMS.COMMIT_AFTERSTATEMENT;
     }
+
+
+
+    public void startDocument(int commitMode)
+        throws SQLException
+    {
+        m_commitMode = commitMode;
+
+        if(m_commitMode == COMMIT_AFTERSTATEMENT)
+            m_connection.setAutoCommit(true);
+
+        // TODO: Do we need to do this for COMMIT_NONE?
+        // It should have already been done!
+        else if(m_commitMode == COMMIT_AFTERDOCUMENT || 
+                m_commitMode == COMMIT_NONE)
+            m_connection.setAutoCommit(false);   
+
+
+    }
+
+    public void endDocument()
+        throws SQLException
+    {
+        if(m_commitMode == COMMIT_AFTERDOCUMENT)
+            m_connection.commit();
+
+        // TODO: If there's an error in the document do we rollback?
+        // My guess is that, if the connection is closed without commit 
+        // then it's automatically rolled back
+    }
+
+
+
+    public abstract void insert(Table table, Row row)
+        throws SQLException; 
 
 
     public void update(Table table, Row row)
         throws SQLException
     {
-        if(doUpdate(table, row) == 0)
+        int ret = doUpdate(table, row);
+
+        if(ret == 0)
             throw new SQLException("[xmldbms] Row to be updated is not present in table.");
-        
-        // TODO: Do we need to check for insane condition where more than one
-        // row matches?
+        else if(ret > 1)
+            throw new SQLException("[xmldbms] Primary key not unique. Multiple rows updated!");
 
         // TODO: Do we need to refresh values here? I think not as any changes 
         // should have been done by us, and updating keys is already suspect.
     }
 
 
-    public abstract void insert(Table table, Row row, boolean soft)
-        throws SQLException;
-
-    
-    public void delete(Table table, Row row, boolean soft)
-        throws SQLException
-    {
-        int ret = doDelete(table, row);
-
-        // TODO: Do we need to check for insane condition where more than one
-        // row matches?
-
-        if(ret == 0 && !soft)
-            throw new SQLException("[xmldbms] Row to be deleted is not present in table.");
-    }
-
     public void updateOrInsert(Table table, Row row)
         throws SQLException
     {
         int ret = doUpdate(table, row);
 
-        // TODO: Do we need to check for insane condition where more than one
-        // row matches?
+        if(ret == 0)
+            insert(table, row);
+        else if(ret > 1)
+            throw new SQLException("[xmldbms] Primary key not unique. Multiple rows updated!");
+    }
+
+
+    public void delete(Table table, Row row)
+        throws SQLException
+    {
+        int ret = doDelete(table, row);
 
         if(ret == 0)
-            insert(table, row, false);
+            throw new SQLException("[xmldbms] Row to be deleted is not present in table.");
+        else if(ret > 1)
+            throw new SQLException("[xmldbms] Primary key not unique. Multiple rows deleted!");
     }
+
        
 
 
@@ -89,9 +123,11 @@ abstract class DBActionImpl
         return stmt.executeUpdate();
     }
 
-    protected int doUpdate(Table table, Row row)
+    protected int doUpdate(Table table, Row row, )
         throws SQLException
     { 
+        // TODO: Do we assume that the parameter
+
         // Get the columns that were set
         Vector cols = row.getColumnVectorFor(table);
     
@@ -105,6 +141,8 @@ abstract class DBActionImpl
             
             cols.remove(priCols[i]);
         }
+
+        // TODO: Remove unique key columns 
 
         // Get trimmed array
         Column[] colArray = (Column[])cols.toArray();
@@ -147,13 +185,10 @@ abstract class DBActionImpl
     protected void setParameters(PreparedStatement stmt, int offset, Object[] values)
         throws SQLException
     {
-        // TODO: Need implementation
         // NOTE: offset is 0 based. Add one to offset to get proper parameter index
 
     }
 
     
 
-    protected Connection m_connection;
-    protected DMLGenerator m_dml;
 }
