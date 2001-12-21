427960e9f3658641215e567cbc276078273ce072
rpbourret <>
2001-12-21 11:50:44 +0000

    move from .xmldbms

---
+++ org/xmlmiddleware/xmldbms/actions/ActionCompiler.java
@@ -0,0 +1,508 @@
+// This software is in the public domain.
+//
+// The software is provided "as is", without warranty of any kind,
+// express or implied, including but not limited to the warranties
+// of merchantability, fitness for a particular purpose, and
+// noninfringement. In no event shall the author(s) be liable for any
+// claim, damages, or other liability, whether in an action of
+// contract, tort, or otherwise, arising from, out of, or in connection
+// with the software or the use or other dealings in the software.
+//
+// Parts of this software were originally developed in the Database
+// and Distributed Systems Group at the Technical University of
+// Darmstadt, Germany:
+//
+//    http://www.informatik.tu-darmstadt.de/DVS1/
+
+// Version 2.0
+// Changes from version 1.x: New in version 2.0
+
+package org.xmlmiddleware.xmldbms.actions;
+
+import org.xmlmiddleware.utils.TokenList;
+import org.xmlmiddleware.utils.XMLName;
+import org.xmlmiddleware.xmldbms.maps.Map;
+import org.xmlmiddleware.xmldbms.maps.PropertyMap;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+/**
+ * Compiles an action document into an Actions object.
+ *
+ * <p>Action documents specify a default action for a document, as well
+ * as per-class (per element type) actions. For more information, see
+ * actions.dtd.</p>
+ *
+ * <p>ActionCompiler assumes that the action document is valid. If
+ * it is not, the class will either generate garbage or throw an exception. One
+ * way to guarantee that the document is valid is to pass a validating parser
+ * to the action compiler.</p>
+ * 
+ * <p>For example, the following code creates an Actions object from the
+ * sales.act action document.</p>
+ *
+ * <pre>
+ *    // Instantiate a new action compiler and set the XMLReader.
+ *    compiler = new ActionCompiler(xmlReader);<br />
+ *
+ *    // Compile sales.act into an Actions object.
+ *    map = compiler.compile(new InputSource(new FileReader("sales.act")));<br />
+ * </pre>
+ *
+ * @author Ronald Bourret, 2001
+ * @version 2.0
+ */
+
+public class ActionCompiler implements ContentHandler
+{
+   //*********************************************************************
+   // Class variables
+   //*********************************************************************
+
+   private XMLReader xmlReader;
+   private TokenList elementTokens;
+   private Actions   actions;
+   private Action    action;
+   private boolean   stateDefaultAction, stateUpdate;
+   private Hashtable uris = new Hashtable(); // Indexed by prefix
+
+   // Debugging variables
+   private int       indent = 0;
+   private boolean   debug = false;
+
+   //*********************************************************************
+   // Constants
+   //*********************************************************************
+
+   // The state gives context for elements that can occur inside more than
+   // one element or that have constraints that cannot be expressed in the
+   // DTD. The following table shows which states affect the processing of
+   // which element types:
+   //
+   // State                  Affected element types
+   // ------------------     -----------------------------------------------------
+   // stateDefaultAction     ElementType, Attribute, PCDATA
+   // stateUpdate            ElementType
+
+   private static String NAMESPACES = "http://xml.org/sax/features/namespaces";
+
+   //*********************************************************************
+   // Constructors
+   //*********************************************************************
+
+   /**
+    * Construct a new ActionCompiler and set the XMLReader (parser).
+    *
+    * @param xmlReader The XMLReader
+    */
+   public ActionCompiler(XMLReader xmlReader)
+      throws SAXException
+   {
+      if (xmlReader == null)
+         throw new IllegalArgumentException("xmlReader argument must not be null.");
+      this.xmlReader = xmlReader;
+      this.xmlReader.setContentHandler(this);
+      this.xmlReader.setFeature(NAMESPACES, true);
+      initTokens();
+   }
+
+   //*********************************************************************
+   // Public methods
+   //*********************************************************************
+
+   /**
+    * Compile an action document into an Actions object.
+    *
+    * @param src A SAX InputSource for the action document.
+    * @param map The map to which the actions apply.
+    * @return The Actions object
+    * @exception SAXException Thrown if the action document contains an error.
+    * @exception IOException Thrown if an IO error occurs.
+    */
+   public Actions compile(Map map, InputSource src)
+      throws SAXException, IOException
+   {
+      // Check the arguments.
+
+      if ((src == null) || (map == null))
+         throw new IllegalArgumentException("map and src arguments must not be null.");
+
+
+      // Initialize global variables.
+
+      stateDefaultAction = false;
+      stateUpdate = false;
+      uris.clear();
+      actions = new Actions(map);
+
+      // Parse the actions document.
+
+      xmlReader.parse(src);
+
+      // Return the Actions object.
+
+      return actions;
+   }
+
+   //**************************************************************************
+   // org.xml.sax.ContentHandler methods
+   //**************************************************************************
+
+   /** For internal use only. */
+   public void startDocument () throws SAXException
+   {
+      if (debug)
+      {
+         System.out.println("Document started.");
+      }
+   }
+
+   /** For internal use only. */
+   public void endDocument() throws SAXException
+   {
+      if (debug)
+      {
+         System.out.println("Document ended.");
+      }
+   }
+
+   /** For internal use only. */
+   public void startElement (String uri, String localName, String qName, Attributes attrs)
+     throws SAXException
+   {
+      
+      // Debugging code.
+      if (debug)
+      {
+         indent();
+         System.out.println(localName + " (start)");
+         indent += 3;
+      }
+
+      if (!uri.equals(ActionConst.URI_ACTIONSV2))
+         throw new SAXException("Unrecognized namespace URI for action language: " + uri);
+
+      switch (elementTokens.getToken(localName))
+      {
+         case ActionConst.ELEM_TOKEN_ACTION:
+            // do nothing
+            break;
+
+         case ActionConst.ELEM_TOKEN_ACTIONS:
+            processActions(attrs);
+            break;
+
+         case ActionConst.ELEM_TOKEN_ALL:
+            throw new SAXException("<All> not yet implemented.");
+//            break;
+
+         case ActionConst.ELEM_TOKEN_ATTRIBUTE:
+            processAttribute(attrs);
+            break;
+
+         case ActionConst.ELEM_TOKEN_DEFAULTACTION:
+            processDefaultAction();
+            stateDefaultAction = true;
+            break;
+
+         case ActionConst.ELEM_TOKEN_DELETE:
+            processAction(Action.DELETE);
+            break;
+
+         case ActionConst.ELEM_TOKEN_ELEMENTTYPE:
+            processElementType(attrs);
+            break;
+
+         case ActionConst.ELEM_TOKEN_INSERT:
+            processAction(Action.INSERT);
+            break;
+
+         case ActionConst.ELEM_TOKEN_NAMESPACE:
+            processNamespace(attrs);
+            break;
+
+         case ActionConst.ELEM_TOKEN_NONE:
+            processAction(Action.NONE);
+            break;
+
+         case ActionConst.ELEM_TOKEN_OPTIONS:
+            // do nothing
+            break;
+
+         case ActionConst.ELEM_TOKEN_PCDATA:
+            processUpdateProperty(null, PropertyMap.PCDATA);
+            break;
+
+         case ActionConst.ELEM_TOKEN_SOFTDELETE:
+            processAction(Action.SOFTDELETE);
+            break;
+
+         case ActionConst.ELEM_TOKEN_SOFTINSERT:
+            processAction(Action.SOFTINSERT);
+            break;
+
+         case ActionConst.ELEM_TOKEN_UPDATE:
+            processAction(Action.UPDATE);
+            stateUpdate = true;
+            break;
+
+         case ActionConst.ELEM_TOKEN_UPDATEORINSERT:
+            processAction(Action.UPDATEORINSERT);
+            break;
+
+         case ActionConst.ELEM_TOKEN_INVALID:
+            throw new SAXException("Unrecognized action language element type: " + localName);
+      }
+   }
+
+   /** For internal use only. */
+   public void endElement (String uri, String localName, String qName) throws SAXException
+   {
+      // Debugging code.
+      if (debug)
+      {
+         indent -= 3;
+         indent();
+         System.out.println(localName + " (end)");
+      }
+
+      if (!uri.equals(ActionConst.URI_ACTIONSV2))
+         throw new SAXException("Unrecognized namespace URI for action language: " + uri);
+
+      switch (elementTokens.getToken(localName))
+      {
+         case ActionConst.ELEM_TOKEN_DEFAULTACTION:
+            stateDefaultAction = false;
+            break;
+
+         case ActionConst.ELEM_TOKEN_UPDATE:
+            stateUpdate = false;
+            break;
+
+         case ActionConst.ELEM_TOKEN_ACTION:
+         case ActionConst.ELEM_TOKEN_ACTIONS:
+         case ActionConst.ELEM_TOKEN_ALL:
+         case ActionConst.ELEM_TOKEN_ATTRIBUTE:
+         case ActionConst.ELEM_TOKEN_DELETE:
+         case ActionConst.ELEM_TOKEN_ELEMENTTYPE:
+         case ActionConst.ELEM_TOKEN_INSERT:
+         case ActionConst.ELEM_TOKEN_NAMESPACE:
+         case ActionConst.ELEM_TOKEN_NONE:
+         case ActionConst.ELEM_TOKEN_OPTIONS:
+         case ActionConst.ELEM_TOKEN_PCDATA:
+         case ActionConst.ELEM_TOKEN_SOFTDELETE:
+         case ActionConst.ELEM_TOKEN_SOFTINSERT:
+         case ActionConst.ELEM_TOKEN_UPDATEORINSERT:
+            // Nothing to do.
+            break;
+
+         case ActionConst.ELEM_TOKEN_INVALID:
+            throw new SAXException("Unrecognized action language element type: " + localName);
+      }
+   }
+
+   /** For internal use only. */
+   public void characters (char ch[], int start, int length)
+     throws SAXException
+   {
+      // No action language elements have character content, so the document is
+      // invalid if any is found (other than whitespace). Since we assume
+      // that the document is valid, don't throw an error.
+   }
+   
+   /** For internal use only. */
+   public void ignorableWhitespace (char ch[], int start, int length)
+      throws SAXException
+   {
+   }
+
+   /** For internal use only. */
+   public void processingInstruction (String target, String data)
+      throws SAXException
+   {
+   }
+
+   /** For internal use only. */
+   public void startPrefixMapping(String prefix, String uri)
+      throws SAXException
+   {
+   }
+
+   /** For internal use only. */
+   public void endPrefixMapping(String prefix)
+      throws SAXException
+   {
+   }
+
+   /** For internal use only. */
+   public void setDocumentLocator (Locator locator)
+   {
+   }
+
+   /** For internal use only. */
+   public void skippedEntity(String name)
+      throws SAXException
+   {
+   }
+
+   //**************************************************************************
+   // Private methods -- element processing methods, in alphabetical order
+   //**************************************************************************
+
+   private void processAction(int actionType)
+   {
+      action.setAction(actionType);
+   }
+
+   private void processActions(Attributes attrs)
+      throws SAXException
+   {
+      String version;
+
+      // Check the version number. Since we can't count on a validating
+      // parser, if the attribute doesn't exist, we assume the version
+      // is correct.
+
+      version = getAttrValue(attrs, ActionConst.ATTR_VERSION, ActionConst.DEF_VERSION);
+      if (!version.equals(ActionConst.DEF_VERSION))
+         throw new SAXException("Unsupported action language version: " + version);
+   }
+
+   private void processAttribute(Attributes attrs)
+      throws SAXException
+   {
+      String qname;
+
+      qname = getAttrValue(attrs, ActionConst.ATTR_NAME);
+      processUpdateProperty(XMLName.create(qname, uris), PropertyMap.ATTRIBUTE);
+   }
+
+   private void processDefaultAction()
+   {
+      action = new Action();
+      actions.setDefaultAction(action);
+   }
+
+   private void processElementType(Attributes attrs)
+      throws SAXException
+   {
+      String qname;
+
+      qname = getAttrValue(attrs, ActionConst.ATTR_NAME);
+
+      if (!stateUpdate)
+      {
+         // The <ElementType> element is inside an <Action> element
+
+         action = actions.createAction(XMLName.create(qname, uris));
+      }
+      else
+      {
+         processUpdateProperty(XMLName.create(qname, uris), PropertyMap.ELEMENTTYPE);
+      }
+   }
+
+   private void processNamespace(Attributes attrs)
+   {
+      String uri, prefix;
+
+      prefix = getAttrValue(attrs, ActionConst.ATTR_PREFIX);
+      if (prefix == null)
+      {
+         // This is a somewhat annoying predicament. In getAttrValue, we set the
+         // attribute value to null if it is an empty string. This is because some
+         // parsers don't follow the SAX spec and incorrectly return an empty string
+         // when the attribute is not found. However, an empty string is a legal value
+         // for the prefix. Therefore, if prefix is null, we assume an empty value was
+         // actually retrieved. We assume that the prefix attribute was present, since
+         // it is required by the DTD and we have undefined behavior if the document
+         // is not valid. Long term, we need to simply fail with parsers that are bogus...
+
+         prefix = "";
+      }
+      uri = getAttrValue(attrs, ActionConst.ATTR_URI);
+
+      uris.put(prefix, uri);
+   }
+
+   private void processUpdateProperty(XMLName propName, int type)
+      throws SAXException
+   {
+      // The <ElementType>, <Attribute>, or <PCDATA> element is inside
+      // an <Update> element.
+
+      if (stateDefaultAction)
+         throw new SAXException("When <Update> is inside <DefaultAction>, the only valid child is <All />.");
+
+      action.setUpdateProperty(propName, type);
+   }
+
+
+   //**************************************************************************
+   // Private methods -- miscellaneous
+   //**************************************************************************
+
+   private void initTokens()
+   {
+      // Set up tokens for element names and enumerated values.
+
+      elementTokens = new TokenList(ActionConst.ELEMS, ActionConst.ELEM_TOKENS, ActionConst.ELEM_TOKEN_INVALID);
+   }
+
+   //**************************************************************************
+   // Private methods -- attribute processing
+   //**************************************************************************
+
+   private String getAttrValue(Attributes attrs, String name)
+   {
+      String value;
+
+      value = attrs.getValue(name);
+
+      // Work-around for parsers (Oracle) that incorrectly return an empty
+      // string instead of a null when the attribute is not found.
+
+      if (value != null)
+      {
+         if (value.length() == 0)
+         {
+            value = null;
+         }
+      }
+
+      return value;
+   }
+
+   private String getAttrValue(Attributes attrs, String name, String defaultValue)
+   {
+      String attrValue;
+
+      // Get the attribute value.
+
+      attrValue = getAttrValue(attrs, name);
+
+      // On null attribute value, return the default.
+
+      return (attrValue == null) ? defaultValue : attrValue;
+   }
+
+   //**************************************************************************
+   // Private methods -- debugging
+   //**************************************************************************
+
+   private void indent()
+   {
+      for (int i = 0; i < indent; i++)
+      {
+         System.out.print(" ");
+      }
+   }
+}
---
+++ org/xmlmiddleware/xmldbms/actions/ActionConst.java
@@ -0,0 +1,167 @@
+// This software is in the public domain.
+//
+// The software is provided "as is", without warranty of any kind,
+// express or implied, including but not limited to the warranties
+// of merchantability, fitness for a particular purpose, and
+// noninfringement. In no event shall the author(s) be liable for any
+// claim, damages, or other liability, whether in an action of
+// contract, tort, or otherwise, arising from, out of, or in connection
+// with the software or the use or other dealings in the software.
+//
+// Parts of this software were originally developed in the Database
+// and Distributed Systems Group at the Technical University of
+// Darmstadt, Germany:
+//
+//    http://www.informatik.tu-darmstadt.de/DVS1/
+
+// Version 2.0
+// Changes from version 1.x: New in version 2.0
+
+package org.xmlmiddleware.xmldbms.actions;
+
+/**
+ * Action language strings and tokens. <b>Only used internally.</b>
+ *
+ * @author Ronald Bourret, 2001
+ * @version 2.0
+ */
+
+public class ActionConst
+{
+   //*********************************************************************
+   // Action attributes and values
+   //*********************************************************************
+
+   public static String URI_ACTIONSV2         = "http://www.xmlmiddleware.org/xmldbms/actions/v2";
+
+/*
+   public static String ATTR_ACTION           = "Action";
+   public static String ATTR_UPDATEPROPERTIES = "UpdateProperties";
+
+   public static String VALUE_NONE            = "None";
+   public static String VALUE_INSERT          = "Insert";
+   public static String VALUE_SOFTINSERT      = "SoftInsert";
+   public static String VALUE_UPDATEORINSERT  = "UpdateOrInsert";
+   public static String VALUE_DELETE          = "Delete";
+   public static String VALUE_SOFTDELETE      = "SoftDelete";
+
+   public static String VALUE_ALL             = "#All";
+   public static String VALUE_PCDATA          = "#PCDATA";
+*/
+
+   //*********************************************************************
+   // Action language element strings and tokens
+   //*********************************************************************
+
+   public static String ELEM_ACTION         = "Action";
+   public static String ELEM_ACTIONS        = "Actions";
+   public static String ELEM_ALL            = "All";
+   public static String ELEM_ATTRIBUTE      = "Attribute";
+   public static String ELEM_DEFAULTACTION  = "DefaultAction";
+   public static String ELEM_DELETE         = "Delete";
+   public static String ELEM_ELEMENTTYPE    = "ElementType";
+   public static String ELEM_INSERT         = "Insert";
+   public static String ELEM_NAMESPACE      = "Namespace";
+   public static String ELEM_NONE           = "None";
+   public static String ELEM_OPTIONS        = "Options";
+   public static String ELEM_PCDATA         = "PCDATA";
+   public static String ELEM_SOFTDELETE     = "SoftDelete";
+   public static String ELEM_SOFTINSERT     = "SoftInsert";
+   public static String ELEM_UPDATE         = "Update";
+   public static String ELEM_UPDATEORINSERT = "UpdateOrInsert";
+   
+   public static final String[] ELEMS = {
+                                         ELEM_ACTION,
+                                         ELEM_ACTIONS,
+                                         ELEM_ALL,
+                                         ELEM_ATTRIBUTE,
+                                         ELEM_DEFAULTACTION,
+                                         ELEM_DELETE,
+                                         ELEM_ELEMENTTYPE,
+                                         ELEM_INSERT,
+                                         ELEM_NAMESPACE,
+                                         ELEM_NONE,
+                                         ELEM_OPTIONS,
+                                         ELEM_PCDATA,
+                                         ELEM_SOFTDELETE,
+                                         ELEM_SOFTINSERT,
+                                         ELEM_UPDATE,
+                                         ELEM_UPDATEORINSERT
+                                        };
+   
+   // Element type tokens
+
+   public static final int ELEM_TOKEN_INVALID        = -999;
+   public static final int ELEM_TOKEN_ACTION         = 0;
+   public static final int ELEM_TOKEN_ACTIONS        = 1;
+   public static final int ELEM_TOKEN_ALL            = 2;
+   public static final int ELEM_TOKEN_ATTRIBUTE      = 3;
+   public static final int ELEM_TOKEN_DEFAULTACTION  = 4;
+   public static final int ELEM_TOKEN_DELETE         = 5;
+   public static final int ELEM_TOKEN_ELEMENTTYPE    = 6;
+   public static final int ELEM_TOKEN_INSERT         = 7;
+   public static final int ELEM_TOKEN_NAMESPACE      = 8;
+   public static final int ELEM_TOKEN_NONE           = 9;
+   public static final int ELEM_TOKEN_OPTIONS        = 10;
+   public static final int ELEM_TOKEN_PCDATA         = 11;
+   public static final int ELEM_TOKEN_SOFTDELETE     = 12;
+   public static final int ELEM_TOKEN_SOFTINSERT     = 13;
+   public static final int ELEM_TOKEN_UPDATE         = 14;
+   public static final int ELEM_TOKEN_UPDATEORINSERT = 15;
+   
+   public static final int[] ELEM_TOKENS = {
+                                            ELEM_TOKEN_ACTION,
+                                            ELEM_TOKEN_ACTIONS,
+                                            ELEM_TOKEN_ALL,
+                                            ELEM_TOKEN_ATTRIBUTE,
+                                            ELEM_TOKEN_DEFAULTACTION,
+                                            ELEM_TOKEN_DELETE,
+                                            ELEM_TOKEN_ELEMENTTYPE,
+                                            ELEM_TOKEN_INSERT,
+                                            ELEM_TOKEN_NAMESPACE,
+                                            ELEM_TOKEN_NONE,
+                                            ELEM_TOKEN_OPTIONS,
+                                            ELEM_TOKEN_PCDATA,
+                                            ELEM_TOKEN_SOFTDELETE,
+                                            ELEM_TOKEN_SOFTINSERT,
+                                            ELEM_TOKEN_UPDATE,
+                                            ELEM_TOKEN_UPDATEORINSERT
+                                           };
+
+   //*********************************************************************
+   // Action language attribute strings and tokens
+   //*********************************************************************
+
+   // Attribute names
+
+   public static String ATTR_NAME             = "Name";
+   public static String ATTR_PREFIX           = "Prefix";
+   public static String ATTR_URI              = "URI";
+   public static String ATTR_VERSION          = "Version";
+   
+   public static final String[] ATTRS = {
+                                         ATTR_NAME,
+                                         ATTR_PREFIX,
+                                         ATTR_URI,
+                                         ATTR_VERSION
+                                        };
+
+   // Attribute tokens
+
+   public static final int ATTR_TOKEN_INVALID = -999;
+   public static final int ATTR_TOKEN_NAME    = 1;
+   public static final int ATTR_TOKEN_PREFIX  = 2;
+   public static final int ATTR_TOKEN_URI     = 3;
+   public static final int ATTR_TOKEN_VERSION = 4;
+
+   public static final int[] ATTR_TOKENS = {
+                                            ATTR_TOKEN_NAME,
+                                            ATTR_TOKEN_PREFIX,
+                                            ATTR_TOKEN_URI,
+                                            ATTR_TOKEN_VERSION
+                                           };
+
+   // Attribute defaults
+
+   public static String DEF_VERSION = "2.0";
+}
